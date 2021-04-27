24526fa79464f53e6f316df81cf8c8929406ced6
Fabian Yamaguchi <fabs@shiftleft.io>
2021-04-27 17:38:19 +0200

    And now it actually works

--- updateQdbJson.sh
+++ updateQdbJson.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-wget https://github.com/joernio/query-database/releases/latest/querydb.json -o static/json/querydb.json
+wget https://github.com/joernio/query-database/releases/latest/download/querydb.json -O static/json/querydb.json
